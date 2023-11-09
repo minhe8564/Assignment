d53db044ad245d66b00e89c75fa07eb466adf82f
minhe8564 <91974925+minhe8564@users.noreply.github.com>
2023-11-09 09:23:19 +0900

    적외선 리모컨 코드 읽기

---
+++ Embedded-software/8.1.ino
@@ -0,0 +1,31 @@
+// 8.1
+// 적외선 리모컨 코드 읽기
+
+#include <IRremote.h>
+
+int irPin = 11;
+
+IRrecv irrecv(irPin);
+
+decode_result results;
+
+void setup() {
+  Serial.begin(9600);
+  
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
+}
+
+void loop() {
+  if(irrecv.decode(&result)){
+    if(result.value != 0xFFFFFFFF){
+      Serial.print("Received Code is ");
+      Serial.println(results.value, HEX);
+      
+      // IR 
+      irrecv printIRResultRawFormatted(&Serial, true);
+      irrecv.rpintIRResultShort(&Serial);
+    };
+    irrecv.resume();
+  }
+}
