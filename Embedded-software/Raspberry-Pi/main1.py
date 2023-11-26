36084f54d7a17f7b9da64af37039fa29a8763cd9
minhe8564 <91974925+minhe8564@users.noreply.github.com>
2023-11-26 18:04:38 +0900

    위험을 알리는 경광등 만들기

---
+++ Embedded-software/Raspberry-Pi/main1.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(16,GPIO.OUT)
+GPIO.setup(20,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+
+try:
+    while 1:
+        GPIO.output(16,GPIO.HIGH)
+        GPIO.output(20,GPIO.HIGH)
+        GPIO.output(21,GPIO.HIGH)
+        time.sleep(1.0)
+        GPIO.output(16,GPIO.LOW)
+        GPIO.output(20,GPIO.LOW)
+        GPIO.output(21,GPIO.LOW)
+        time.sleep(1.0)
+except KeyboardInterrupt:
+    pass
+  
+GPIO.cleanup()
